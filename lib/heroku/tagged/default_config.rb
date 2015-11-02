b8803ca2e37bf4e432088fdd23cc15a8fb49abf7
David Westerink <davidakachaos@gmail.com>
2015-11-02 17:14:11 +0100

    ???

--- lib/heroku/tagged/default_config.rb
+++ lib/heroku/tagged/default_config.rb
@@ -1,13 +1,15 @@
 require 'heroku_san'
-module HerokuSan
-  class Configuration
-    def initialize_with_tagged(configurable, stage_factory = HerokuSan::Stage)
-      initialize_without_tagged(configurable, stage_factory = HerokuSan::Stage)
-      @options[:deploy] = HerokuSan::Deploy::Tagged
-      @options['deploy'] = HerokuSan::Deploy::Tagged
-    end
+if defined?(HerokuSan)
+  module HerokuSan
+    class Configuration
+      def initialize_with_tagged(configurable, stage_factory = HerokuSan::Stage)
+        initialize_without_tagged(configurable, stage_factory = HerokuSan::Stage)
+        @options[:deploy] = HerokuSan::Deploy::Tagged
+        @options['deploy'] = HerokuSan::Deploy::Tagged
+      end
 
-    alias_method :initialize_without_tagged, :initialize
-    alias_method :initialize, :initialize_with_tagged
+      alias_method :initialize_without_tagged, :initialize
+      alias_method :initialize, :initialize_with_tagged
+    end
   end
 end
