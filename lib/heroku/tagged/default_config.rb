4a033d1fbfe5f1fe17e806954dc5bdd643464c83
David Westerink <davidakachaos@gmail.com>
2015-11-04 14:41:19 +0100

    Up

--- lib/heroku/tagged/default_config.rb
+++ lib/heroku/tagged/default_config.rb
@@ -3,7 +3,11 @@ if defined?(HerokuSan)
   module HerokuSan
     class Configuration
       def initialize_with_tagged(configurable, stage_factory = HerokuSan::Stage)
-        initialize_without_tagged(configurable, stage_factory = HerokuSan::Stage)
+        begin
+          initialize_without_tagged(configurable, stage_factory = HerokuSan::Stage)
+        rescue ArgumentError
+          initialize_without_tagged(configurable)
+        end
         @options[:deploy] = HerokuSan::Deploy::Tagged
         @options['deploy'] = HerokuSan::Deploy::Tagged
       end
--- lib/heroku/tagged/deploy/version.rb
+++ lib/heroku/tagged/deploy/version.rb
@@ -1,7 +1,7 @@
 module Heroku
   module Tagged
     module Deploy
-      VERSION = "0.0.2.5"
+      VERSION = "0.0.2.6"
     end
   end
 end
