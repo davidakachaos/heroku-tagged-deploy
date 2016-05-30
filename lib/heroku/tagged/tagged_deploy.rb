940819f3b0913c5ef7e1bcc3f184a33ac8188ddb
David Westerink <davidakachaos@gmail.com>
2016-05-30 12:35:51 +0200

    Fixes

--- lib/heroku/tagged/tagged_deploy.rb
+++ lib/heroku/tagged/tagged_deploy.rb
@@ -3,54 +3,57 @@ module HerokuSan
     class Tagged < Base
 
       def get_changes
-        puts "Getting current tags..."
-        sh "git fetch --tags"
-        puts "[#{@stage.name}] Fetching current state."
-        sh "git fetch #{@stage.name}"
-        puts "[#{@stage.name}] Deploying..."
+        say "Getting current tags..."
+        `git fetch --tags`
+        say "Fetching current state."
+        `git fetch #{@stage.name}`
+        say "Deploying..."
         previous_sha_commit = `git rev-parse HEAD`.strip
-        puts "[#{@stage.name}] Deploying these changes:"
+        say "Deploying these changes:"
         puts `git log --pretty=format:"* %s" #{@stage.name}/master..#{previous_sha_commit}`
-        puts ""
         @file_changes = `git diff --name-only #{previous_sha_commit} #{@stage.name}/master`
       end
 
       def enable_maintenance_if_needed
         if @file_changes.index("db/migrate") || @file_changes.index("db/seeds.rb")
-          puts "[#{@stage.name}] Migrations found -- enabling maintenance mode..."
-          @stage.maintenance(:on)
+          say "Migrations found -- enabling maintenance mode..."
+          `heroku maintenance:on -a #{@stage.app}`
         end
       end
 
       def post_changes
         if @file_changes.index("db/migrate")
-          puts "[#{@stage.name}] Running migrations, as there are new ones added."
-          @stage.migrate
+          say "Running migrations, as there are new ones added."
+          `heroku run rake db:migrate -a #{@stage.app}`
         end
         if @file_changes.index("db/seeds.rb")
-          puts "[#{@stage.name}] It looks like you adjusted or created the seeds.rb file."
-          puts "[#{@stage.name}] Running the rake db:seed command now..."
-          @stage.run('rake db:seed')
+          say "It looks like you adjusted or created the seeds.rb file."
+          say "Running the rake db:seed command now..."
+          `heroku run rake db:seed -a #{@stage.app}`
         end
         if @file_changes.index("db/migrate") || @file_changes.index("db/seeds.rb")
-          puts "[#{@stage.name}] Disabling the maintenance mode."
-          @stage.maintenance(:off)
-          puts "[#{@stage.name}] Restarting..."
-          @stage.restart
+          say "Disabling the maintenance mode."
+          `heroku maintenance:off -a #{@stage.app}`
+          say "Restarting..."
+          `heroku restart -a #{@stage.app}`
         end
       end
 
       def precompile_assets
-        sh 'rake RAILS_ENV=production RAILS_GROUP=assets assets:precompile'
-        sh 'rake RAILS_GROUP=assets assets:clean_expired'
+        `rake RAILS_ENV=production RAILS_GROUP=assets assets:precompile`
+        `rake RAILS_GROUP=assets assets:clean_expired`
         if system('git diff --cached --exit-code') == false || system('git diff --exit-code') == false
-          sh 'git add -A .'
-          sh 'git gc'
-          sh "git commit -m 'assets precompiled'"
-          sh 'git push'
+          `git add -A .`
+          `git gc`
+          `git commit -m 'assets precompiled'`
+          `git push`
         else
-          puts "No assets changed, no extra commit needed."
+          say "No assets changed, no extra commit needed."
         end
+      end
+
+      def say(text)
+        puts "[#{Time.now.strftime('%H:%M %S')}][#{@stage.name}] #{text}"
       end
 
       def deploy
